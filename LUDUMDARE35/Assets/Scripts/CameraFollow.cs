5c0a3b194d565af26874602027033260a1c09d4f
Erin McBride <emcbride750@gmail.com>
2016-04-16 10:24:16 -0700

    camerais neat now

--- LUDUMDARE35/Assets/Scripts/CameraFollow.cs
+++ LUDUMDARE35/Assets/Scripts/CameraFollow.cs
@@ -1,31 +1,57 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+[RequireComponent(typeof(Camera))]
 public class CameraFollow : MonoBehaviour {
 
     public Transform target;
     public float damping = 0.2f;
 
-    public float m_OffsetZ;
+    //private float m_OffsetZ;
+    private float m_Size;
+    private Camera cam;
+    public float zoomSpeed = 0.7f;
+    public float maxZoomOutRelative = 1.4f;
     private Vector3 m_CurrentVelocity;
-
+    //private float maxAxisLookAhead = 5.0f;
     // Use this for initialization
     private void Start()
     {
-        m_OffsetZ = (transform.position - target.position).z;
-        transform.parent = null;
+        //m_OffsetZ = (transform.position - target.position).z;
+        m_Size = this.cam.orthographicSize;
+        //transform.parent = null;
     }
 
+    private void Awake()
+    {
+        this.cam = this.GetComponent<Camera>();
+        cam.orthographic = true;
+    }
+
+    private static float velocityDamp(float v)
+    {
+        if (v >= 0.0f)
+        {
+            return Mathf.Sqrt(Mathf.Abs(v));
+        }
+        else
+        {
+            return -1.0f * Mathf.Sqrt(Mathf.Abs(v));
+        }
+    }
 
     // Update is called once per frame
     private void Update()
     {
-
-        Vector3 aheadTargetPos = new Vector3(target.position.x, target.position.y, target.position.z);
-        Vector3 currentPos = new Vector3(transform.position.x, transform.position.y, target.position.z);
+        Vector2 velocity = target.GetComponent<Rigidbody2D>().velocity;
+        float vDamp = velocityDamp(velocity.magnitude);
+        Vector3 aheadTargetPos = new Vector3(target.position.x + velocityDamp(velocity.x), target.position.y + velocityDamp(velocity.y), 0);
+        Vector3 currentPos = new Vector3(transform.position.x, transform.position.y, 0);
         Vector3 midPos = Vector3.SmoothDamp(currentPos, aheadTargetPos, ref m_CurrentVelocity, damping);
 
+        float zoom = Mathf.Lerp(m_Size, Mathf.Max(m_Size, Mathf.Min(m_Size * maxZoomOutRelative, m_Size * vDamp * zoomSpeed)), Time.time);
+        this.cam.orthographicSize = zoom;
 
-        transform.position = midPos + (Vector3.forward * m_OffsetZ);
+
+        transform.position = midPos + (transform.position.z * Vector3.forward);
     }
 }
